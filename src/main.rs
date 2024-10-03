b742df5af25db280b875639d9aaffb1b82f901e3
Eric Schmale <eric.schmale@gmx.de>
2024-10-03 15:20:17 +0200

    update file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use chrono::prelude::*;
 use std::fs;
 use std::sync::Arc;
 use tokio::sync::RwLock;
+use tokio::time::{sleep, Duration};
 
 mod article_search;
 
@@ -16,20 +17,39 @@ async fn main() {
     println!("Reading data");
     let start_time = Local::now();
 
-    let data = fs::read_to_string("./json/artikel.json").expect("Can't read file");
-
-    let data: String = data
-        .chars()
-        .filter(|c| (c >= &'!' && c <= &'ÿ') || c == &' ')
-        .collect();
-
-    let art_all = article_search::ArtikelListe::from_json(&data);
+    let art_all: article_search::ArtikelListe = Default::default();
 
     let shared_state = Arc::new(RwLock::new(art_all));
 
+    // For background task
+    let reload_state = Arc::clone(&shared_state);
+    // Spawn a background task to reload the file every 10 minutes
+    tokio::spawn(async move {
+        loop {
+            // Reload the JSON file
+            if let Ok(data) = fs::read_to_string("./json/artikel.json") {
+                let data: String = data
+                    .chars()
+                    .filter(|c| (c >= &'!' && c <= &'ÿ') || c == &' ')
+                    .collect();
+
+                let artikel_list = article_search::ArtikelListe::from_json(&data);
+
+                // Update the shared state (write lock needed here)
+                let mut write_guard = reload_state.write().await;
+                *write_guard = artikel_list;
+
+                tracing::info!("--artikel.json reloaded");
+            } else {
+                tracing::error!("Failed to reload artikel.json");
+            }
+            sleep(Duration::from_secs(5)).await;
+        }
+    });
+
     let end_time = Local::now();
     let duration = end_time.signed_duration_since(start_time);
-    println!("Reading Data took: {:?}", duration);
+    tracing::info!("Reading Data took: {:?}", duration);
 
     println!("Starting server");
     tracing_subscriber::fmt::init();
@@ -46,7 +66,7 @@ async fn main() {
 }
 
 async fn root() -> &'static str {
-    "Hello, World!"
+    "Search Service"
 }
 
 async fn search_article(
